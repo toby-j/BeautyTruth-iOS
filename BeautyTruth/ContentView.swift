4dbd8017a66be6e198095a7ab8bc3bd91eb90df7
Toby Johnson <tobyjohnson2000@gmail.com>
2020-06-02 21:12:25 +0100

    Initial Commit

---
+++ BeautyTruth/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BeautyTruth
+//
+//  Created by TobyDev on 02/06/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
